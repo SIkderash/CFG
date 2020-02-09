b2eb1bbb130f783f777cbc2c15925f77761769a5
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-02-09 20:08:48 +0600

    Update SourceCodeReader.java

--- SourceCodeReader.java
+++ SourceCodeReader.java
@@ -23,43 +23,22 @@ public class SourceCodeReader {
         Scanner scan = new Scanner(file);
         ArrayList<String> Lines = new ArrayList<>();
         ArrayList<String> Statements = new ArrayList<>();
-        ArrayList<Node>Graph = new ArrayList<>();
+      
         AllMethods Al = new AllMethods();
         
-        int statementNo = 1;
-        Node parentNode= null;
-        Node ParentOfBranch = null;
-            while (scan.hasNextLine()) {
+        
+        while (scan.hasNextLine()) {
                 String currentLine = scan.nextLine();
                 Lines.add(currentLine);
-                if(Al.isStatement(currentLine)){
-                    Statements.add(currentLine);
-                    Node node = new Node(statementNo, currentLine);
-                    node.parents.add(parentNode);
-                    parentNode.childs.add(node);
-                    parentNode = node;
-                    Graph.add(node);
-                    statementNo++;
-                }
-                else if(true){
-                    if(Al.isIf(currentLine)){
-                        ParentOfBranch = parentNode;
-                        while(scan.hasNextLine()){
-                            
-                        }
-                    }
-                }
+                
             }
+         makeGraph MG = new makeGraph(Lines);   
            
             
 
             /*for (int i = 0; i < Statements.size(); i++) {
                System.out.println(Statements.get(i));
             }*/
-            for (int i = 0; i < Graph.size(); i++) {
-                System.out.print("Node: "+ Graph.get(i).nodeNumber+"\t");
-                System.out.println(Graph.get(i).Statement+ "\n");
-            }
             
             
      
