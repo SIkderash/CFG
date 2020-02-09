b9ad5a40c8bdd55bd2cca032c3c2de91bb64c078
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-02-09 20:08:09 +0600

    Update MakeGraph.java

--- MakeGraph.java
+++ MakeGraph.java
@@ -6,26 +6,73 @@
 package cfg;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  *
  * @author sikde
  */
-public class MakeGraph {
-        ArrayList<String> Statements = new ArrayList<>();
-        ArrayList<Node>Graph = new ArrayList<>();
-        int iterator = 0;
-        
-        AllMethods Al = new AllMethods();
+public class makeGraph {
 
-        public MakeGraph(ArrayList<String> Lines){
-            while(iterator<Lines.size()){
-                iterator++;
-                String currentLine = Lines.get(iterator);
-                //System.out.println(currentLine+"\n");
-                if(currentLine)
-                
-            }
+    ArrayList<String> Graph = new ArrayList<>();
+    ArrayList<String> Lines = new ArrayList<>();
+    int statementNo = 1;
+    Node parentNode = null;
+    Node parentOfBranch = null;
+    int iterator_1 = 0, iterator_2 = 0;
+    AllMethods Al = new AllMethods();
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public makeGraph(ArrayList<String> Lins) {
+        for (int i = 0; i < Lins.size(); i++) {
+            this.Lines.add(Lins.get(i));
         }
-        
+    }
+
+    public void makeRelations()
+    {
+        while (iterator_1 < Lines.size()) {
+
+            String currentLine = Lines.get(iterator_1);
+            iterator_1++;
+            if (Al.isStatement(currentLine)) {
+                Node node = new Node(statementNo, currentLine);
+                node.parents.add(parentNode);
+                parentNode.childs.add(node);
+                parentNode = node;
+                Graph.add(currentLine);
+                statementNo++;
+            }
+            //System.out.println(currentLine+"\n");
+            if (Al.isIf(currentLine)) {
+                parentOfBranch = parentNode;
+                stack.push(parentOfBranch.nodeNumber);
+                while (!stack.empty()) {
+                    currentLine = Lines.get(iterator_1);
+                    iterator_1++;
+                    if(Al.foundEnd(currentLine)){
+                        stack.pop();
+                        continue;
+                    }
+                    else if (Al.isStatement(currentLine)) {
+                        Node node = new Node(statementNo, currentLine);
+                        parentNode = new Node(stack.peek(),Graph.get(stack.peek()));
+                        node.parents.add(parentNode);
+                        parentNode.childs.add(node);
+                        parentNode = node;
+                        Graph.add(currentLine);
+                        statementNo++;
+                    }
+                    else if(Al.isIf(currentLine))
+                }
+
+            }
+
+        }
+        for (int i = 0; i < Graph.size(); i++) {
+            System.out.print(Graph.get(i)+ "\t");
+        }
+
+    }
+
 }
