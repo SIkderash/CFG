5a127bef75194d8833a39f3f6f8ad5774a970795
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-02-26 12:20:03 +0600

    Update and rename AllMethods.java to SyntaxChecker.java

--- SyntaxChecker.java
+++ SyntaxChecker.java
@@ -10,7 +10,7 @@ import java.util.regex.PatternSyntaxException;
  *
  * @author sikde
  */
-public class AllMethods {
+public class SyntaxChecker {
     
     
     public boolean isStatement(String s){
