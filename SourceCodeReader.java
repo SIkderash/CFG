2642de68dcef36a5fec0cc5aa4b23f39d5ae7ca5
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-03-11 17:09:40 +0600

    Refactored

--- SourceCodeReader.java
+++ SourceCodeReader.java
@@ -32,7 +32,7 @@ public class SourceCodeReader {
                 Lines.add(currentLine);
                 
             }
-         makeGraph MG = new makeGraph(Lines);   
+         makeGraph Graph = new makeGraph(Lines);   
            
             
 
