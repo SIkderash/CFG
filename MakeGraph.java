cf8eb0f27cec9be23db320b90346681c9fb0ffeb
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-05-10 11:23:38 +0600

    Update MakeGraph.java

--- MakeGraph.java
+++ MakeGraph.java
@@ -6,6 +6,8 @@
 package cfg;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -21,7 +23,8 @@ public class MakeGraph {
     SyntaxChecker CheckSyntax = new SyntaxChecker();
     Stack stackOfParentNodes = new Stack();
     Node root;
-    ArrayList<Node>graph = new ArrayList<>();
+    ArrayList<ArrayList<Node> >graph = new ArrayList<ArrayList<Node>>();
+    Queue<Node> q = new LinkedList<>();
     
 
     /**
@@ -50,41 +53,53 @@ public class MakeGraph {
          stackOfParentNodes.add(root);
          Node currentNode = root;
          System.out.println(currentNode.nodeNumber + "->  " + currentNode.Statement);
-         Node BranchParentStored = root;
+         Node branchParentStored = root;
+         ArrayList<Node>branchLeaves = new ArrayList<>();
+         boolean inBranch = false;
         while (!stackOfParentNodes.empty()) {
-           
+            
             currentLine = Lines.get(iteratorOfLines);
             if (CheckSyntax.isStatement(currentLine)) {
-               
                
                 Node node = new Node(++statementNo, currentLine);
                 Node parent = currentNode;
                 parent.childs.add(node);
                 node.parents.add(parent);
                 currentNode = node;
+                if(!inBranch && !branchLeaves.isEmpty() ){
+                    for(int i=0; i<branchLeaves.size(); i++){
+                        node.parents.add(branchLeaves.get(i));
+                    }
+                }
                 System.out.println(currentNode.nodeNumber + "->  " + currentNode.Statement);
             }
             else if(CheckSyntax.foundEnd(currentLine)){
-                BranchParentStored = (Node) stackOfParentNodes.peek();
-                System.out.println("BranchParentStored " + BranchParentStored.nodeNumber + "->  " + BranchParentStored.Statement);
+                branchParentStored = (Node) stackOfParentNodes.peek();
+                System.out.println("BranchParentStored " + branchParentStored.nodeNumber + "->  " + branchParentStored.Statement);
                 stackOfParentNodes.pop();
                 System.out.println(currentLine);
+                if(inBranch==true){
+                    branchLeaves.add(currentNode);
+                }
             }
             //System.out.println(currentLine+"\n");
             else if (CheckSyntax.isIf(currentLine)) {
+                inBranch = true;
                 stackOfParentNodes.add(currentNode);
-                BranchParentStored = (Node) stackOfParentNodes.peek();
+                branchParentStored = (Node) stackOfParentNodes.peek();
                 System.out.println("if->  " + currentLine);
             }
             else if(CheckSyntax.isElseIf(currentLine)){
-                stackOfParentNodes.add(BranchParentStored);
-                currentNode = BranchParentStored;
+                inBranch = true;
+                stackOfParentNodes.add(branchParentStored);
+                currentNode = branchParentStored;
                 System.out.println("else if->  " + currentLine);
                
             }
             else if(CheckSyntax.isElse(currentLine)){
-                stackOfParentNodes.add(BranchParentStored);
-                currentNode = BranchParentStored;
+                inBranch = true;
+                stackOfParentNodes.add(branchParentStored);
+                currentNode = branchParentStored;
                 System.out.println("else->  " + currentLine);
                
             }
@@ -106,159 +121,37 @@ public class MakeGraph {
 
     }
     public void printGraph(){
+        //System.out.print(root.Statement + "\n");
+        if(root!=null){q.add(root);}
         print(root);
-         int[] graphIndexedWithParents = new int[graph.size()];
-        for(int i=1; i<this.graph.size(); i++){
-            //graphIndexedWithParents[i] = this.graph.get(i).parents.get(0).nodeNumber;
-            //System.out.print(graphIndexedWithParents[i]+" ");
-            //System.out.print(this.graph.get(i).parents.get(0).nodeNumber);
-        }
         
     }
     public void print(Node traversingNode){
        //System.out.println("y");
-      
-         if(!traversingNode.childs.isEmpty()){
-         for(int i=0; i<traversingNode.childs.size(); i++){
-             Node child = traversingNode.childs.get(i);
-             System.out.print(child.parents.get(0).nodeNumber);
-             this.graph.add(traversingNode);
-             print(child);
-         }   
-        }
-    }
-    
-
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cfg;
-
-import java.util.ArrayList;
-import java.util.Stack;
-
-/**
- *
- * @author sikde
- */
-public class MakeGraph {
-    
-    ArrayList<String> Lines = new ArrayList<>();   //Canprint all nodes
-    
-    int iteratorOfLines = 0;
-    int statementNo ;
-    SyntaxChecker CheckSyntax = new SyntaxChecker();
-    Stack stackOfParentNodes = new Stack();
-    Node root;
-    ArrayList<Node>graph = new ArrayList<>();
-    
-
-    /**
-     *
-     * @param LinesOfTheSourceCode
-     */
-    public MakeGraph(ArrayList<String> LinesOfTheSourceCode) {
-        for (int i = 0; i < LinesOfTheSourceCode.size(); i++) {
-            this.Lines.add(LinesOfTheSourceCode.get(i));
-            System.out.println(Lines.get(i));
-        }
-    }
-
-    public void makeRelations()
-    {
-          statementNo =0;
-         String currentLine = Lines.get(iteratorOfLines);
-         while(!CheckSyntax.isStatement(currentLine) && iteratorOfLines<Lines.size()){
-            
-             currentLine = Lines.get(iteratorOfLines);
-              iteratorOfLines++;
-     
+       Node imaginary_root = new Node(-1,"");
+       imaginary_root.childs.add(root);                                                                                                      ;
+       ArrayList<Node> temp1 = new ArrayList<>();
+       temp1.add(imaginary_root);
+       graph.add(temp1);
+       while(!q.isEmpty()){
+           ArrayList<Node> temp = new ArrayList<>();
+         for(int i=0; i<q.peek().childs.size(); i++){
+             temp.add(q.peek().childs.get(i));
+             q.add(q.peek().childs.get(i));
          }
-          
-         root = new Node(statementNo,currentLine);
-         stackOfParentNodes.add(root);
-         Node currentNode = root;
-         System.out.println(currentNode.nodeNumber + "->  " + currentNode.Statement);
-         Node BranchParentStored = root;
-        while (!stackOfParentNodes.empty()) {
-           
-            currentLine = Lines.get(iteratorOfLines);
-            if (CheckSyntax.isStatement(currentLine)) {
-               
-               
-                Node node = new Node(++statementNo, currentLine);
-                Node parent = currentNode;
-                parent.childs.add(node);
-                node.parents.add(parent);
-                currentNode = node;
-                System.out.println(currentNode.nodeNumber + "->  " + currentNode.Statement);
-            }
-            else if(CheckSyntax.foundEnd(currentLine)){
-                BranchParentStored = (Node) stackOfParentNodes.peek();
-                System.out.println("BranchParentStored " + BranchParentStored.nodeNumber + "->  " + BranchParentStored.Statement);
-                stackOfParentNodes.pop();
-                System.out.println(currentLine);
-            }
-            //System.out.println(currentLine+"\n");
-            else if (CheckSyntax.isIf(currentLine)) {
-                stackOfParentNodes.add(currentNode);
-                BranchParentStored = (Node) stackOfParentNodes.peek();
-                System.out.println("if->  " + currentLine);
-            }
-            else if(CheckSyntax.isElseIf(currentLine)){
-                stackOfParentNodes.add(BranchParentStored);
-                currentNode = BranchParentStored;
-                System.out.println("else if->  " + currentLine);
-               
-            }
-            else if(CheckSyntax.isElse(currentLine)){
-                stackOfParentNodes.add(BranchParentStored);
-                currentNode = BranchParentStored;
-                System.out.println("else->  " + currentLine);
-               
-            }
-            
-            else if(CheckSyntax.isFor(currentLine)){
-                stackOfParentNodes.add(currentNode);
-                System.out.println("for->  " + currentLine);
-            }
-            else if(CheckSyntax.isWhile(currentLine)){
-                stackOfParentNodes.add(currentNode);
-                System.out.println("while->  " + currentLine);
-            }
-             iteratorOfLines++;
-
-        }
-        
-        
-        
-
+         Node remNode = q.poll();
+         graph.add(temp);
     }
-    public void printGraph(){
-        print(root);
-         int[] graphIndexedWithParents = new int[graph.size()];
-        for(int i=1; i<this.graph.size(); i++){
-            //graphIndexedWithParents[i] = this.graph.get(i).parents.get(0).nodeNumber;
-            //System.out.print(graphIndexedWithParents[i]+" ");
-            //System.out.print(this.graph.get(i).parents.get(0).nodeNumber);
-        }
-        
+       for(int i=0; i<graph.size(); i++){
+           System.out.print(i + "\t");
+           for(int j=1; j<graph.get(i).size(); j++){
+              System.out.print(graph.get(i).get(j).nodeNumber + " ");
+           }
+           System.out.println();
+       }
+       DrawGraph DG = new DrawGraph(graph);
+       DG.draw();
+       
     }
-    public void print(Node traversingNode){
-       //System.out.println("y");
-      
-         if(!traversingNode.childs.isEmpty()){
-         for(int i=0; i<traversingNode.childs.size(); i++){
-             Node child = traversingNode.childs.get(i);
-             System.out.print(child.parents.get(0).nodeNumber);
-             this.graph.add(traversingNode);
-             print(child);
-         }   
-        }
-    }
-    
 
 }
