59ab80fb3ee478ab7ced654a31f948d8db78a8bf
SIkderash <48786809+SIkderash@users.noreply.github.com>
2020-05-10 17:41:42 +0600

    Buttons added

--- DrawGraph.java
+++ DrawGraph.java
@@ -17,36 +17,72 @@ import java.util.Queue;
 
 public class DrawGraph {
      public ArrayList<ArrayList<Node>>graph = new  ArrayList<>(); 
+     
+     
     public DrawGraph(ArrayList<ArrayList<Node>> graph){
           this.graph.addAll(graph);
     }
+    
+    
     public void draw(){
+        
         JFrame f1 = new JFrame("Control Flow Graph");
         f1.setBackground(Color.BLUE);
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel p1 = new JPanel();
-        p1.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        int tempx = 500, tempy = 100, width = 100, height = 20;
-        gbc.gridx = 500;
-        gbc.gridy = 50; 
-        Queue<Node>q = new LinkedList<>();
-        //System.out.print(q.peek().nodeNumber);
-        q.add(graph.get(0).get(0));
         
-        System.out.print(q.peek().nodeNumber + "*");
+        int constx = 0, consty = 10, width = 10, height = 5;
+        
+        Queue<Node>q = new LinkedList<>();
+        for(int i=0; i<graph.size(); i++){
+            System.out.print(i+"-> ");
+             for(int j=0; j<graph.get(i).size(); j++)
+             {
+                 System.out.print(graph.get(i).get(j).nodeNumber+" ");
+             }
+             
+             System.out.print("\n");
+         }
+        
+        q.add(graph.get(1).get(0));
+              
+        System.out.print(graph.get(1).get(0).nodeNumber);
+        JButton b = new JButton(Integer.toString(graph.get(1).get(0).nodeNumber));
+                b.setBackground(Color.yellow);
+                b.setBounds(constx, consty, (int)width, height);
+                p1.add(b);
+        int x = constx, y = consty;
         while(!q.isEmpty()){
-            for(int i=0; i<graph.get(q.peek().nodeNumber).size(); i++)
-            {
-                JButton node = new JButton(Integer.toString(q.peek().nodeNumber));
-                p1.add(node);
-            }
+            int i;
+            int node_num = q.peek().nodeNumber;
+            System.out.print(q.peek().nodeNumber+"\n");
             
+            for(i=0; i<graph.get(node_num+2).size(); i++){
+                JButton button = new JButton(Integer.toString(graph.get(node_num+2).get(i).nodeNumber));
+                button.setBackground(Color.yellow);
+                button.setBounds(x, y, (int) width, height);
+                p1.add(button);
+                q.add(graph.get(node_num+2).get(i));
+                if(i==graph.get(node_num+2).size()/2){
+                    i++;
+                    break;
+                }
+                x-=10;
+            }
+            for(i=i; i<graph.get(node_num+2).size(); i++){
+                JButton button = new JButton(Integer.toString(graph.get(node_num+2).get(i).nodeNumber));
+                button.setBackground(Color.yellow);
+                button.setBounds(x, y, (int) width, height);
+                p1.add(button);
+                q.add(graph.get(node_num+2).get(i));
+                x+=10;
+            }
+            q.poll();
+            y+=120;
         }
         
         f1.add(p1);
-        
         f1.setSize(2000,900);
         f1.setVisible(true);
     }
