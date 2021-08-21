c1ba17779f8894f5d245b1f3cf6fa4b930915e7d
SIkderash <48786809+SIkderash@users.noreply.github.com>
2021-08-21 13:12:32 +0600

    EdgeList now readable from files

--- ConnectedNodes.java
+++ ConnectedNodes.java
@@ -4,82 +4,221 @@
  * and open the template in the editor.
  */
 package cfg;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
- * @author Asus
+ * @author sikde
  */
-
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.stage.Stage;
-import java.util.Random;
-
-class graphicNode extends Circle {
-    public double dragBaseX;
-    public double dragBaseY;
-
-    public graphicNode(double centerX, double centerY, double radius) {
-        super(centerX, centerY, radius);
-
-        setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                setCenterX(event.getSceneX() - dragBaseX);
-                setCenterY(event.getSceneY() - dragBaseY);
-            }
-        });
-    }
-}
-
-class Edge extends Line {
-    public Edge(graphicNode startNode, graphicNode endNode) {
-        startXProperty().bind(startNode.centerXProperty());
-        startYProperty().bind(startNode.centerYProperty());        
-        endXProperty().bind(endNode.centerXProperty());
-        endYProperty().bind(endNode.centerYProperty());        
-    }
-}
-
-public class ConnectedNodes extends Application {
-    public int[][] Adj;
-    @Override
-    public void start(Stage stage) throws Exception {
-        Group root = new Group();
-
-        initGraph(root);
-
-        Scene scene = new Scene(root, 400, 400);
-        stage.setScene(scene);
-        stage.show();
+public class MakeGraph {
+    
+    ArrayList<String>Lines;
+    ArrayList<ArrayList<Integer> > nodesInLevel = new ArrayList<>();
+    boolean vis[];
+    int[][] adj = new int[50][50];
+    SyntaxChecker checker = new SyntaxChecker();
+    int cur = 0;
+    //Constructor to pass the lines of code
+    public MakeGraph (ArrayList<String> lines){
+        this.Lines = lines;
+        vis = new boolean[Lines.size()];
+        //for(int i=0; i<Lines.size(); i++) System.out.println(i+ " " + Lines.get(i));
     }
     
-    private void initGraph(Group root) {
+    public void start() throws IOException{
+        cur=0;
         
-        double R = 20;
-        Random rnd = new Random();
-        for(int i=0; i<10; i++){
-            graphicNode node1 = new graphicNode(rnd.nextDouble()*300,rnd.nextDouble()*300, R);
-            node1.setFill(Color.RED);
-            root.getChildren().add(node1);
-            graphicNode node2 = new graphicNode(rnd.nextDouble()*300,rnd.nextDouble()*300, R);
-            node2.setFill(Color.RED);
-            root.getChildren().add(node2);
-
-            Edge connection = new Edge(node1, node2);
-            connection.setStroke(Color.CYAN);
-            connection.setStrokeWidth(5);
-            root.getChildren().add(0, connection);
+        while(Lines.get(cur).contains("int main(){") || Lines.get(cur).charAt(0)=='#'){
+            cur++;
+        }
+        
+        Node root = new Node(cur,Lines.get(cur));
+        cur++;
+        
+        System.out.println("root node no: " + root.nodeNumber +"\n"+ "root node statement: "+root.Statement);
+        
+        makeRelations(root, false);
+        
+        dfs(root, -1);
+        printGraph();
+        saveGraph();
+        //DrawGraph draw = new DrawGraph();
+        //nodesInLevel = draw.drawGraphWithAjacencyMatrix(adj,cur+1,root.nodeNumber);
+        //System.out.println("nodesInLevel has currently size "+ nodesInLevel.size());
+        ConnectedNodes drawGraph = new ConnectedNodes();
+        drawGraph.main();
+    }
+    
+    
+    public Node makeRelations(Node branchRoot, boolean inLoop){
+        Node par = branchRoot;
+        //System.out.println(cur);
+        ArrayList<Node> branchingsOfThisBranch = new ArrayList<>();
+        //System.out.println(branchRoot.nodeNumber+" "+inLoop);
+        
+        while(cur<Lines.size()) {
+        //System.out.println("finished " + cur);
+        Node curNode = new Node(cur,Lines.get(cur));
+        
+        if(checker.isElse(curNode.Statement)){
+            //System.out.println("Else - "+ curNode.Statement);
+            
+            par.childs.add(curNode);
+            cur++;
+            branchingsOfThisBranch.add(makeRelations(curNode, false));
+        }
+        
+        
+        
+        
+        
+        
+        
+        else if(checker.isElseIf(curNode.Statement)){
+            //System.out.println("Else If - "+ curNode.Statement);
+            
+            par.childs.add(curNode);
+            cur++;
+            branchingsOfThisBranch.add(makeRelations(curNode, false));
+        }
+        
+        
+        
+        
+        
+        
+        else if(checker.isIf(curNode.Statement)){
+            //System.out.println("If - "+ curNode.Statement);
+            
+            if(branchingsOfThisBranch.size()>0){
+                for(int i=0; i<branchingsOfThisBranch.size(); i++){
+                    branchingsOfThisBranch.get(i).childs.add(curNode);
+                    branchingsOfThisBranch.clear();
+                }
+            }
+            else{
+                par.childs.add(curNode);
+            }
+            cur++;
+            branchingsOfThisBranch.add(makeRelations(curNode, false));
+        }
+        
+        
+        
+        
+        
+        
+        else if(checker.isLoop(curNode.Statement)){
+            //System.out.println("loop - "+ curNode.Statement);
+            
+            if(branchingsOfThisBranch.size()>0){
+                for(int i=0; i<branchingsOfThisBranch.size(); i++){
+                    branchingsOfThisBranch.get(i).childs.add(curNode);
+                    branchingsOfThisBranch.clear();
+                }
+            }
+            else{
+                par.childs.add(curNode);
+            }
+            branchingsOfThisBranch.add(curNode);
+            cur++;
+            makeRelations(curNode, true);
+        }
+        
+        
+        
+        
+        
+        
+        else{
+            //System.out.println("Statement - "+ curNode.Statement+branchingsOfThisBranch.size());
+             if(branchingsOfThisBranch.size()>0){
+                for(int i=0; i<branchingsOfThisBranch.size(); i++){
+                    branchingsOfThisBranch.get(i).childs.add(curNode);
+                }
+                branchingsOfThisBranch.clear();
+            }
+            else{
+                par.childs.add(curNode);
+            }
+            //branchingsOfThisBranch.add(curNode);
+            cur++;
+            if(checker.foundEnd(curNode.Statement)){
+                if(inLoop==true) {
+                    //System.out.println(curNode.nodeNumber);
+                    //System.out.println(branchRoot);
+                    curNode.childs.add(branchRoot);
+                }
+                return curNode;
+            }
+            par = curNode;
+        }
+           
+        
+        
+        
+            
+        
+        }
+        return null;
+    }
+    
+    
+    
+    public void dfs(Node cur, int prev){
+        //System.out.println(prev + " " + cur.nodeNumber+" "+cur.Statement);
+        vis[cur.nodeNumber] = true;
+        
+        for(int i=0; i<cur.childs.size(); i++){
+            int nodeNo = cur.childs.get(i).nodeNumber;
+            if(vis[nodeNo]==false){
+                dfs(cur.childs.get(i), cur.nodeNumber);
+            }
+        }
+        
+        for(int i=0; i<cur.childs.size(); i++){
+            adj[cur.nodeNumber][cur.childs.get(i).nodeNumber] = 1;
         }
     }
-
-    public static void main(String... args) {
-        launch(args);
+    
+    public void printGraph (){
+        System.out.println("\nAdjacency List:");
+        for(int i=0; i<Lines.size(); i++){
+            System.out.print("\t"+i+"  ->   ");
+            for(int j=0; j<Lines.size(); j++){
+                if(adj[i][j]==1){
+                    System.out.print(j+" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("\nAdjacency Matrix:");
+        for(int i=0; i<Lines.size(); i++){
+            System.out.print("\t"+i+"\t");
+            for(int j=0; j<Lines.size(); j++){
+                System.out.print(adj[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
+    public void saveGraph() throws IOException{
+        FileWriter myWriter = new FileWriter("F:\\Downloads\\CFG-master\\Edges.txt");
+        
+        myWriter.write((Lines.size())+"\n");
+        for(int i=0; i<Lines.size(); i++){
+            for(int j=0; j<Lines.size(); j++){
+                if(adj[i][j]==1){
+                    myWriter.write(i+" "+j+"\n");
+                }
+            }
+        }
+        
+        myWriter.close();
+    }
+    
 }
