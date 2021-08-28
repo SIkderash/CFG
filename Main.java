a3f82708998c598a61c90df35e108bb50c3cbd8f
SIkderash <48786809+SIkderash@users.noreply.github.com>
2021-08-29 04:35:29 +0600

    Unused import removed and Refactored

--- Main.java
+++ Main.java
@@ -6,19 +6,21 @@
 package cfg;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 /**
  *
  * @author sikde
  */
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Control Flow Graph\n\n");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+       System.out.println("Control Flow Graph\n\n");
        System.out.println("Enter the directory of a C source code file\n");
-       String fileName;
+       String fileName;                         // F:\\Downloads\\CFG-master\\test.txt
+       Scanner input = new Scanner(System.in);
+       fileName = input.next();
        SourceCodeReader Sc;                
-       Sc = new SourceCodeReader("F:\\Downloads\\CFG-master\\test.txt");
+       Sc = new SourceCodeReader(fileName);
     }  
 }
